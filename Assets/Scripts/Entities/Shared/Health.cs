97f3de28f9abec7e6dd17ad95afc33f312b712f9
Grizzlelab <104280850+Grizzlelab@users.noreply.github.com>
2022-05-21 18:48:29 -0500

    Add Heal/Damage methods

--- Assets/Scripts/Entities/Shared/Health.cs
+++ Assets/Scripts/Entities/Shared/Health.cs
@@ -1,10 +1,21 @@
+using System;
 using UnityEngine;
 
 namespace Kitsuma.Entities.Shared
 {
     public class Health : MonoBehaviour
     {
-        private float maxHealth = 10f;
-        private float currentHealth = 10f;
+        [SerializeField] private float maxHealth = 10f;
+        [SerializeField] private float currentHealth = 10f;
+
+        public void Heal(float amount)
+        {
+            Math.Clamp(currentHealth + amount, 0f, maxHealth);
+        }
+
+        public void Damage(float amount)
+        {
+            Math.Clamp(currentHealth - amount, 0f, maxHealth);
+        }
     }
 }

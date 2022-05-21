95a8f5dd3ce2b098545f07dd0448740027276e78
Grizzlelab <104280850+Grizzlelab@users.noreply.github.com>
2022-05-21 18:26:14 -0500

    WIP health

---
+++ Assets/Scripts/Entities/Shared/Health.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Kitsuma.Entities.Shared
+{
+    public class Health : MonoBehaviour
+    {
+        private float maxHealth = 10f;
+        private float currentHealth = 10f;
+    }
+}

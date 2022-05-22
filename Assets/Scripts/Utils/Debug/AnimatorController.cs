6f3ba22395d1237ad2a9dbce85063a27cf3834b9
Grizzlelab <104280850+Grizzlelab@users.noreply.github.com>
2022-05-22 16:50:19 -0500

    Create dev environment

---
+++ Assets/Scripts/Utils/Debug/AnimatorController.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+namespace Kitsuma.Utils.Debug
+{
+    public class AnimatorController : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text tmp;
+        
+        private Animator _anim;
+        private string _state;
+
+        private void Awake()
+        {
+            _anim = GetComponent<Animator>();
+        }
+
+        public void ShowAnimation()
+        {
+            if (string.Equals(_state, tmp.text)) return;
+            // We have to remove the trailing whitespace from the input field
+            string cleaned = tmp.text.Replace("\u200B", "");
+            _state = cleaned;
+            _anim.Play(cleaned);
+        }
+    }
+}

c111429099d3b086c99eef209b81851be001e9ad
Grizzlelab <104280850+Grizzlelab@users.noreply.github.com>
2022-05-23 16:55:48 -0500

    Create rotating necro indicator

---
+++ Assets/Scripts/Utils/RotateTowards.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Kitsuma.Utils
+{
+    public class RotateTowards : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private float speed = 5f;
+
+        private Transform _t;
+
+        private void Awake()
+        {
+            _t = transform;
+        }
+
+        private void Update()
+        {
+            transform.rotation = Quaternion.Slerp(
+                _t.rotation, 
+                GetRotation(), 
+                speed * Time.deltaTime);
+        }
+
+        private Quaternion GetRotation()
+        {
+            return Quaternion.AngleAxis(GetAngle(), Vector3.forward);
+        }
+
+        private float GetAngle()
+        {
+            Vector2 dir = GetDirection();
+            return Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+        }
+
+        private Vector2 GetDirection()
+        {
+            return (target.position - _t.position).normalized;
+        }
+    }
+}

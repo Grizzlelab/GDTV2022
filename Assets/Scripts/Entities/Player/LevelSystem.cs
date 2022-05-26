b8ceb678e8f12a198d0ea9096f236f276b07cdf1
Grizzlelab <104280850+Grizzlelab@users.noreply.github.com>
2022-05-25 23:18:02 -0500

    Add exp orb giving exp

--- Assets/Scripts/Entities/Player/LevelSystem.cs
+++ Assets/Scripts/Entities/Player/LevelSystem.cs
@@ -18,6 +18,7 @@ namespace Kitsuma.Entities.Player
         {
             if (currentLevel >= maxLevel) return;
             _exp += exp;
+            Debug.Log($"{_exp} / {GetExperienceForLevel(currentLevel + 1)}");
             onExpGained?.Invoke(_exp, GetExperienceForLevel(currentLevel + 1));
             if (GetExperienceForLevel(currentLevel + 1) < _exp) return;
             _exp -= GetExperienceForLevel(currentLevel - 1);
--- Assets/Scripts/Utils/OnCollide.cs
+++ Assets/Scripts/Utils/OnCollide.cs
@@ -8,11 +8,14 @@ namespace Kitsuma.Utils
     {
         [SerializeField] private UnityEvent<Transform> onCollide;
         [SerializeField] private string targetTag = "Player";
+        [SerializeField] private bool destroyOnCollide;
 
         private void OnTriggerEnter2D(Collider2D col)
         {
             if (!col.CompareTag(targetTag)) return;
             onCollide?.Invoke(col.transform);
+            if (!destroyOnCollide) return;
+            Destroy(gameObject);
         }
     }
 }

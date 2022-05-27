4d3e97dfd1de243cb597617a9eecdfb5fec57610
Grizzlelab <104280850+Grizzlelab@users.noreply.github.com>
2022-05-27 00:38:50 -0500

    Create bat mob

---
+++ Assets/Scripts/Movement/FollowPlayer.cs
@@ -0,0 +1,40 @@
+using Kitsuma.Managers;
+using UnityEngine;
+
+namespace Kitsuma.Movement
+{
+    public class FollowPlayer : MonoBehaviour
+    {
+        [SerializeField] private float speed = 7f;
+
+        private Transform _t;
+        private Transform _player;
+
+        private void Awake()
+        {
+            _t = transform;
+        }
+
+        private void Start()
+        {
+            _player = GameManager.Instance.GetPlayer().transform;
+        }
+
+        private void Update()
+        {
+            if (IsAtPlayer()) return;
+            Vector3 dir = GetDirectionToPlayer();
+            _t.position += dir * (speed * Time.deltaTime);
+        }
+
+        private Vector3 GetDirectionToPlayer()
+        {
+            return (_player.position - _t.position).normalized;
+        }
+
+        private bool IsAtPlayer()
+        {
+            return Vector3.Distance(_player.position, _t.position) <= 0.5f;
+        }
+    }
+}

e80a355ecf2fb786b59950fd96f5e3b08e582d68
Grizzlelab <104280850+Grizzlelab@users.noreply.github.com>
2022-05-27 23:21:45 -0500

    Create ground clutter prefabs

---
+++ Assets/Scripts/Utils/RandomScale.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Kitsuma.Utils
+{
+    public class RandomScale : MonoBehaviour
+    {
+        [SerializeField] private Vector3 minScale = Vector3.one;
+        [SerializeField] private Vector3 maxScale = Vector3.one * 3;
+
+        private Transform _t;
+
+        private void Awake()
+        {
+            _t = transform;
+        }
+
+        private void Start()
+        {
+            _t.localScale = new Vector3(
+                Random.Range(minScale.x, maxScale.x),
+                Random.Range(minScale.y, maxScale.y),
+                Random.Range(minScale.z, maxScale.z));
+        }
+    }
+}

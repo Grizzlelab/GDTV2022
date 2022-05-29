52ebbc8f9938a5f54cd88b3a3d6ea3cf185976a8
Grizzlelab <104280850+Grizzlelab@users.noreply.github.com>
2022-05-29 04:20:01 -0500

    Hide health bars when max HP

--- Assets/Scripts/Entities/Shared/HealthBar.cs
+++ Assets/Scripts/Entities/Shared/HealthBar.cs
@@ -5,10 +5,28 @@ namespace Kitsuma.Entities.Shared
     public class HealthBar : MonoBehaviour
     {
         [SerializeField] private Transform bar;
+        [SerializeField] private Transform background;
+        [SerializeField] private Transform border;
+        
+        [SerializeField] private bool hideOnFull;
+
+        private void Start()
+        {
+            if (hideOnFull) Hide();
+        }
 
         public void SetFill(float fillNormalized)
         {
             bar.localScale = new Vector3(fillNormalized, 1f);
+            if (hideOnFull) Hide();
+        }
+
+        private void Hide()
+        {
+            Vector3 localScale = bar.localScale;
+            bar.gameObject.SetActive(localScale.x < 1f);
+            background.gameObject.SetActive(localScale.x < 1f);
+            border.gameObject.SetActive(localScale.x < 1f);
         }
     }
 }

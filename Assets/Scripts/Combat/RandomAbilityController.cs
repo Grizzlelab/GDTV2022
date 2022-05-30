c7a8bb4458202e21ada6d44273088bae15058614
Grizzlelab <104280850+Grizzlelab@users.noreply.github.com>
2022-05-30 03:57:22 -0500

    Create main and instruction menus

---
+++ Assets/Scripts/Combat/RandomAbilityController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Kitsuma.Combat
+{
+    public class RandomAbilityController : MonoBehaviour
+    {
+        [SerializeField] private float minRandTime = 5f;
+        [SerializeField] private float maxRandTime = 10f;
+
+        private Transform _t;
+        private AbilityManager _abilities;
+        private bool _isOnCooldown;
+
+        private void Awake()
+        {
+            _t = transform;
+            _abilities = GetComponent<AbilityManager>();
+        }
+
+        private void Update()
+        {
+            if (_isOnCooldown) return;
+            _abilities.UseAbilities(_t.position + (Vector3)Random.insideUnitCircle);
+            StartCoroutine(Wait());
+        }
+
+        private IEnumerator Wait()
+        {
+            _isOnCooldown = true;
+            yield return new WaitForSeconds(Random.Range(minRandTime, maxRandTime));
+            _isOnCooldown = false;
+        }
+    }
+}
